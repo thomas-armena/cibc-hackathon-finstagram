bff7d9446edbed4072a418d33e4ce9354bccda5a
thomas-armena <thomasarmena@gmail.com>
2018-12-01 17:16:17 -0500

    renamed folder to openbanking, added sidebar

---
+++ openbanking/src/App.js
@@ -0,0 +1,85 @@
+import React, { Component } from 'react';
+//import menuBar from 'components/menuBar';
+import LiveFeed from './pages/Livefeed';
+import Insight from './pages/Insight';
+import Pool from './pages/Pool';
+import axios from 'axios';
+import './App.css';
+
+class App extends Component {
+	constructor(props){
+		super(props)
+		this.state = {
+			content:<LiveFeed/>,
+			sidebarActive: false,
+		}
+		this.handleHomeClick = this.handleHomeClick.bind(this);
+		this.handleInsightClick = this.handleInsightClick.bind(this);
+		this.handlePoolClick = this.handlePoolClick.bind(this);
+		this.handleMenuClick = this.handleMenuClick.bind(this);
+
+	}
+
+	componentDidMount(){
+		axios.defaults.headers.common['Ocp-Apim-Subscription-Key'] = '5396d28023f147439c63ef73571705c6'
+		axios.get("https://api.azureminilab.com/customers/1/accounts")
+			.then(response => { console.log(response) })
+			.catch(error => {console.log(error) })
+	}
+
+	handleHomeClick(){
+		this.setState({content: <LiveFeed />});
+	}
+
+	handleInsightClick(){
+		this.setState({content: <Insight />});
+	}
+
+	handlePoolClick(){
+		this.setState({content: <Pool />});
+	}
+
+	handleMenuClick(){
+		//window.alert('check')
+		this.setState({sidebarActive: !this.state.sidebarActive})
+	}
+
+	render() {
+		let contentToggle = this.state.sidebarActive ? "sbActive" : "sbInactive";
+		let contentClass = "app-content "+contentToggle;
+
+		let sidebarToggle = this.state.sidebarActive ? "show" : "hide";
+		let sidebarClass = "sidebar "+sidebarToggle;
+
+		return (
+			<div className="App">
+				{/*Sidebar*/}
+				<div className={sidebarClass}>
+					test
+				</div>
+
+				<div className={contentClass}>
+					{/*Menu Bar */}
+					<div className="menubar">
+						<div className="menubar-title">finstagram</div>
+						<div className="menubar-menu"><i onClick={this.handleMenuClick} className="material-icons">menu</i></div>
+					</div>
+
+					{/*Content*/}
+					<div className="content">
+						{this.state.content}
+					</div>
+
+					{/*Navigation Bar */}
+					<div className="navbar">
+						<div className="navbar-item"><i onClick={this.handleInsightClick} className="material-icons">bar_chart</i></div>
+						<div className="navbar-item"><i onClick={this.handleHomeClick} className="material-icons">home</i></div>
+						<div className="navbar-item"><i onClick={this.handlePoolClick} className="material-icons">people</i></div>
+					</div>
+				</div>
+			</div>
+		);
+	}
+}
+
+export default App;
