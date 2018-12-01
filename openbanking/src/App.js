76d0135a602186eaf361940d31da2a067d64cd2c
thomas-armena <thomasarmena@gmail.com>
2018-12-01 17:41:45 -0500

    Fixed merge

--- openbanking/src/App.js
+++ openbanking/src/App.js
@@ -10,11 +10,13 @@ class App extends Component {
 	constructor(props){
 		super(props)
 		this.state = {
-			content:<LiveFeed/>
+			content:<LiveFeed/>,
+			sidebarActive: false,
 		}
 		this.handleHomeClick = this.handleHomeClick.bind(this);
 		this.handleInsightClick = this.handleInsightClick.bind(this);
 		this.handlePoolClick = this.handlePoolClick.bind(this);
+		this.handleMenuClick = this.handleMenuClick.bind(this);
 
 	}
 
@@ -37,25 +39,43 @@ class App extends Component {
 		this.setState({content: <Pool />});
 	}
 
+	handleMenuClick(){
+		//window.alert('check')
+		this.setState({sidebarActive: !this.state.sidebarActive})
+	}
+
 	render() {
+		let contentToggle = this.state.sidebarActive ? "sbActive" : "sbInactive";
+		let contentClass = "app-content "+contentToggle;
+
+		let sidebarToggle = this.state.sidebarActive ? "show" : "hide";
+		let sidebarClass = "sidebar "+sidebarToggle;
+
 		return (
 			<div className="App">
-				{/*Menu Bar */}
-				<div className="menubar">
-					<div className="menubar-menu"><i className="material-icons">menu</i></div>
-					<div className="menubar-title">finstagram</div>
+				{/*Sidebar*/}
+				<div className={sidebarClass}>
+					test
 				</div>
 
-				{/*Content*/}
-				<div className="content">
-					{this.state.content}
-				</div>
+				<div className={contentClass}>
+					{/*Menu Bar */}
+					<div className="menubar">
+						<div className="menubar-title">finstagram</div>
+						<div className="menubar-menu"><i onClick={this.handleMenuClick} className="material-icons">menu</i></div>
+					</div>
 
-				{/*Navigation Bar */}
-				<div className="navbar">
-					<div className="navbar-item"><i onClick={this.handleInsightClick} className="material-icons">bar_chart</i></div>
-					<div className="navbar-item"><i onClick={this.handleHomeClick} className="material-icons">home</i></div>
-					<div className="navbar-item"><i onClick={this.handlePoolClick} className="material-icons">people</i></div>
+					{/*Content*/}
+					<div className="content">
+						{this.state.content}
+					</div>
+
+					{/*Navigation Bar */}
+					<div className="navbar">
+						<div className="navbar-item"><i onClick={this.handleInsightClick} className="material-icons">bar_chart</i></div>
+						<div className="navbar-item"><i onClick={this.handleHomeClick} className="material-icons">home</i></div>
+						<div className="navbar-item"><i onClick={this.handlePoolClick} className="material-icons">people</i></div>
+					</div>
 				</div>
 			</div>
 		);
